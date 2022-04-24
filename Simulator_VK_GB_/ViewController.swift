4c74dd74e467a67bde9cedd5f1ab0d3f0e059feb
Егор Анатольевич <egorkaefimenko@gmail.com>
2022-04-24 23:16:40 +0300

    Initial Commit

---
+++ Simulator_VK_GB_/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simulator_VK_GB_
+//
+//  Created by Egor Efimenko on 24.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
